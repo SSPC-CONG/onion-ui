a4fa8c0b6d8af6a35f23473b2fddced8458ba3f7
cong.peng <cong.peng@lavieinno.com>
2021-10-13 15:26:41 +0800

    init

---
+++ onions-ui/packages/Message/src/message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="message">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-message',    // 申明组件的 name属性
+  props: {
+    message: String
+  },
+  method:{
+
+  },
+  mounted(){
+    alert(message)
+  }
+}
+</script>
